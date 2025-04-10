5be1b4afd24af499c720cbe64239f73116ed4326
Maynagashev <lifesup@gmail.com>
2025-04-11 01:26:17 +0700

    Настройка базового HTTP-сервера с Chi и middleware

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,6 +1,63 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	defaultReadTimeout  = 10 * time.Second
+	defaultWriteTimeout = 10 * time.Second
+	defaultIdleTimeout  = 30 * time.Second
+	defaultServerPort   = "8080"
+)
+
 // main - точка входа для сервера GophKeeper.
 func main() {
-	// Реализация будет добавлена позже
+	log.Println("Запуск сервера GophKeeper...")
+
+	// Создаем новый роутер
+	r := chi.NewRouter()
+
+	// Используем стандартные middleware для логирования, восстановления после паник и т.д.
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)    // Логирование запросов
+	r.Use(middleware.Recoverer) // Восстановление после паник
+
+	// Базовый маршрут для проверки работы сервера
+	r.Get("/ping", func(w http.ResponseWriter, _ *http.Request) {
+		_, err := w.Write([]byte("pong\n"))
+		if err != nil {
+			log.Printf("Ошибка записи ответа: %v", err)
+		}
+	})
+
+	// TODO: Добавить маршруты для регистрации и логина
+	// r.Post("/register", handlers.RegisterUser)
+	// r.Post("/login", handlers.LoginUser)
+
+	// Задаем порт сервера (можно вынести в конфигурацию)
+	port := defaultServerPort
+	log.Printf("Сервер слушает на порту %s", port)
+
+	// Создаем и настраиваем HTTP-сервер с таймаутами
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%s", port),
+		Handler:      r,
+		ReadTimeout:  defaultReadTimeout,  // Таймаут чтения запроса
+		WriteTimeout: defaultWriteTimeout, // Таймаут записи ответа
+		IdleTimeout:  defaultIdleTimeout,  // Таймаут простоя соединения
+	}
+
+	// Запускаем HTTP-сервер
+	log.Printf("Запуск HTTP-сервера на %s", server.Addr)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Ошибка запуска сервера: %v", err)
+	}
 }
--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,12 @@
 module github.com/maynagashev/gophkeeper/server
 
 go 1.24.2
+
+require github.com/go-chi/chi/v5 v5.2.1
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+)
